d1e78611708cc2a7d8ba7432c260bcea874020a7
Oleksandr Havlytskyi <havlytskyi@gmail.com>
2023-04-05 15:51:23 +0300

    added PERFORMANCE_TEST

--- AppAnagram/AppAnagram/Program.cs
+++ AppAnagram/AppAnagram/Program.cs
@@ -1,5 +1,4 @@
-﻿#define PERFORMANCE_TEST
-
+﻿#undef PERFORMANCE_TEST
 using System;
 using System.Diagnostics;
 
@@ -9,31 +8,37 @@ namespace AppAnagram
     {
         static void Main(string[] args)
         {
-            Anagram anagram = new Anagram(); 
             Console.WriteLine(Properties.Messages.Greeting);
-            Console.Write(Properties.Messages.AskTextForAnagram);
-            string? text = Console.ReadLine();
-            string result = anagram.Reverse(text);
-            Console.WriteLine(result);
+            Console.WriteLine(Properties.Messages.AskTextForAnagram);
 #if PERFORMANCE_TEST
-
-            var performanceTest = new Anagram();
-            var iterationCount = 100_000;
-
-            var stopwatch = Stopwatch.StartNew();
+            Anagram performanceTest = new Anagram();
+            int iterationCount = 100_000;
+            Stopwatch stopwatch = Stopwatch.StartNew();
             stopwatch.Start();
             for (int i = 0; i < iterationCount; i++)
             {
-                performanceTest.Reverse("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisi varius ex laoreet egestas ac nec orci. Vestibulum gravida purus ut arcu imperdiet, non sollicitudin metus sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec cursus ipsum a tortor feugiat, vitae feugiat massa luctus. Aliquam id quam ac enim tincidunt dignissim. In hac habitasse platea dictumst. Praesent vel quam nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Morbi eget est quis dolor aliquet rhoncus.Duis suscipit vehicula nunc.Phasellus sollicitudin sapien in sollicitudin lobortis.Vivamus imperdiet velit augue, et consequat diam pellentesque at.Sed eget bibendum eros, eu sodales turpis.Donec bibendum imperdiet sem.Nam interdum nibh ut justo vulputate, vel hendrerit tellus egestas.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Curabitur lobortis malesuada libero vitae ornare.Fusce suscipit, lectus sit amet pharetra vel.");
+                performanceTest.Reverse(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                    "Sed ac nisi varius ex laoreet egestas ac nec orci. " +
+                    "Vestibulum gravida purus ut arcu imperdiet, non sollicitudin metus sodales. " +
+                    "Interdum et malesuada fames ac ante ipsum primis in faucibus. " +
+                    "Donec cursus ipsum a tortor feugiat, vitae feugiat massa luctus. " +
+                    "Aliquam id quam ac enim tincidunt dignissim. " +
+                    "In hac habitasse platea dictumst. Praesent vel quam nisi. " +
+                    "Pellentesque habitant morbi tristique senectus et netus " +
+                    "et malesuada fames ac turpis egestas.Morbi eget est quis dolor aliquet " +
+                    "rhoncus.Duis suscipit vehicula nunc.Phasellus sollicitudin sapien in " +
+                    "sollicitudin lobortis.Vivamus imperdiet velit augue, et consequat " +
+                    "diam pellentesque at.Sed eget bibendum eros, eu sodales turpis.Donec bibendum " +
+                    "imperdiet sem.Nam interdum nibh ut justo vulputate, vel hendrerit tellus " +
+                    "egestas.Orci varius natoque penatibus et magnis dis parturient montes, nascetur " +
+                    "ridiculus mus.Curabitur lobortis malesuada libero vitae ornare.Fusce suscipit, " +
+                    "lectus sit amet pharetra vel.");
             }
             stopwatch.Stop();
             Console.WriteLine(
             $"Iteration count: {iterationCount}\n" +
-            $"time: {stopwatch.Elapsed.ToString(@"m\:ss\.fff")}\n" +
-            $"avrg.: {stopwatch.Elapsed.TotalMilliseconds / iterationCount}");
-
-            return;
-#pragma warning disable CS0162
+            $"time: {stopwatch.Elapsed.ToString(@"m\:ss\.fff")}");
 #endif
         }
     }
